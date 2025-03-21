c33be16197d6d891c0d0216e1912d0350b134696
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2025-03-21 21:35:01 +0100

    :lipstick: Fixed an issue with badges not being coloured correctly

--- resources/views/livewire/invoice-details.blade.php
+++ resources/views/livewire/invoice-details.blade.php
@@ -1,9 +1,9 @@
 @php
     $statusClasses = [
-        'paid' => 'bg-green-100 text-green-800',
-        'unpaid' => 'bg-yellow-100 text-yellow-800',
-        'overdue' => 'bg-red-100 text-red-800',
-        'draft' => 'bg-gray-100 text-gray-800',
+        'paid' => 'bg-green-200 text-green-900',
+        'unpaid' => 'bg-yellow-200 text-yellow-900',
+        'overdue' => 'bg-red-200 text-red-900',
+        'draft' => 'bg-gray-200 text-gray-900',
     ];
     $statusText = [
         'paid' => 'Betalt',
